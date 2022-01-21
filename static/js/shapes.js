1a0e00414eaa39e78ea76ad55e90a74460112942
lewisaaronpaul <lewisaaronpaul@gmail.com>
2022-01-21 10:59:01 -0600

    remove touch eventListener

--- static/js/shapes.js
+++ static/js/shapes.js
@@ -119,14 +119,14 @@ document.addEventListener("click", function(event) {
     World.add(engine.world, [shape])
 })
 
-// On click, add a new shape
-document.addEventListener("touchstart", function(event) {
+// // On click, add a new shape
+// document.addEventListener("touchstart", function(event) {
 
-    const shape = createShape(event.pageX, event.pageY)
+//     const shape = createShape(event.pageX, event.pageY)
 
-    // initialShapes.bodies.push(shape)
-    World.add(engine.world, [shape])
-})
+//     // initialShapes.bodies.push(shape)
+//     World.add(engine.world, [shape])
+// })
 
 
 // When we move our mouse, check matter for any collision
